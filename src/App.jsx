b316e0b61534144a4da700d19c547ecf30d0721e
MinKhantKyaw <minkhant050603@gmail.com>
2025-07-06 12:27:53 +0700

    Update App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from 'gsap/all'
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+import React from 'react'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Cocktails from './components/Cocktails';
+import About from './components/About';
+import Art from './components/Art';
+import Menu from './components/Menu';
+import Contact from './components/Contact';
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Cocktails />
+      <About />
+      <Art />
+      <Menu />
+      <Contact />
+    </main>
+  );
+};
+
+export default App;
